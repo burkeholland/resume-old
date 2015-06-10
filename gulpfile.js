0c2d74745a36cae95dde26a85f886f6dbc6e9803
Burke Holland <burkeholland@gmail.com>
2015-06-10 14:36:17 -0500

    adding breakpoints

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,36 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('css', function() {
-  watch('./custom.css', function() {
-    gulp.src(['components/pure/base.css', 
-              'components/pure/grids.css', 
-              'css/fontello.css',
-              './custom.css'])
-      .pipe(concat('style.css'))
-      .pipe(gulp.dest('./css'))
-      .pipe(minifyCSS())
-      .pipe(rename('style.min.css'))
-      .pipe(gulp.dest('./css'));
-  });
+var sassSrc = './src/sass/**/*.{scss,sass}';
+
+gulp.task('sass', function() {
+  gulp.src(sassSrc)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./src/css'));
 });
-  
 
-gulp.task('default', ['css']);
+gulp.task('concat', ['sass'], function() {
+  gulp.src('./src/css/**/*.css')
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify', ['concat'], function() {
+  gulp.src('./css/style.css')
+    .pipe(minifyCSS())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(sassSrc, ['sass', 'concat', 'minify']);
+});  
+
+gulp.task('default', ['watch']);
