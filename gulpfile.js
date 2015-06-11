3af903f6aadb79ce3f5fe74491e7e73d14d98692
Burke Holland <burkeholland@gmail.com>
2015-06-11 16:16:00 -0500

    smoosh css inline onto page

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var smoosher = require('gulp-smoosher');
 
 var sassSrc = './src/sass/**/*.{scss,sass}';
 
@@ -31,8 +32,33 @@ gulp.task('minify', ['concat'], function() {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(sassSrc, ['sass', 'concat', 'minify']);
-});  
+gulp.task('smoosh', ['minify'], function() {
+  gulp.src('./src/index.html')
+    .pipe(smoosher())
+    .pipe(gulp.dest('./'));
+});
 
-gulp.task('default', ['watch']);
+// gulp.task('build', function() {
+//   gulp.src(sassSrc)
+//     .pipe(sourcemaps.init())
+//     .pipe(sass({
+//       errLogToConsole: true
+//     }))
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest('./src/css'))
+//     .pipe(concat('./src/css/**/*.css'))
+//     .pipe(gulp.dest('./css'))
+//     .pipe(minifyCSS())
+//     .pipe(rename('style.min.css'))
+//     .pipe(gulp.dest('./css'))
+//     .pipe(gulp.src('./css/style.min.css'))
+//     .pipe(inject('./index.html'));
+// });
+
+gulp.task('build', ['sass', 'concat', 'minify', 'smoosh']);
+
+gulp.task('watch', function() {
+  gulp.watch(sassSrc, ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
