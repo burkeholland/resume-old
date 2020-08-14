52a04fce73256f465514637c09e68741799cb171
Burke Holland <burkeholland@gmail.com>
2020-08-14 14:45:18 -0500

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,94 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var watch = require('gulp-watch');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var smoosher = require('gulp-smoosher');
-var wait = require('gulp-wait');
-var sassSrc = './src/sass/**/*.{scss,sass}';
-var stylesSrc = './src/css/**/*.css';
+// var gulp = require("gulp");
+// var concat = require("gulp-concat");
+// var minifyCSS = require("gulp-minify-css");
+// var rename = require("gulp-rename");
+// var watch = require("gulp-watch");
+// var sass = require("gulp-sass");
+// var sourcemaps = require("gulp-sourcemaps");
+// var smoosher = require("gulp-smoosher");
+// var wait = require("gulp-wait");
 
-gulp.task('sass', function() {
-  gulp.src(sassSrc)
-    .pipe(sourcemaps.init())
-    .pipe(wait(500))
-    .pipe(sass({
-      errLogToConsole: true,
-      outputStyle: 'compressed',
-      includePaths: ['./node_modules/susy/sass']
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./src/css'));
-});
+// var sassSrc = "./src/sass/**/*.{scss,sass}";
+// var stylesSrc = "./src/css/**/*.css";
 
-gulp.task('concat', ['sass'], function() {
-  gulp.src(stylesSrc)
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./css'));
-});
+// sass.compiler = require("node-sass");
 
-gulp.task('minify', ['concat'], function() {
-  gulp.src('./css/style.css')
-    .pipe(minifyCSS())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./css'));
-});
+// function sass() {
+//   return gulp
+//     .src(sassSrc)
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(gulp.dest("./build/css"));
+// }
 
-gulp.task('smoosh', ['minify'], function() {
-  gulp.src('./src/index.html')
-    .pipe(smoosher())
-    .pipe(gulp.dest('./'));
-});
+// function minify() {
+//   // }, gulp.series('concat'), function() {
+//   gulp
+//     .src("./css/style.css")
+//     .pipe(minifyCSS())
+//     .pipe(rename("style.min.css"))
+//     .pipe(gulp.dest("./css"));
+// }
 
-gulp.task('build', ['sass', 'concat', 'minify', 'smoosh']);
+// function smoosh() {
+//   // }, gulp.series('minify'), function() {
+//   gulp.src("./src/index.html").pipe(smoosher()).pipe(gulp.dest("./"));
+// }
 
-gulp.task('watch', function() {
-  gulp.watch([sassSrc, "./src/index.html"], ['build']);
-});
+// function watch() {
+//   // , function() {
+//   gulp.watch([sassSrc, "./src/index.html"], ["build"]);
+// }
 
-gulp.task('default', ['build', 'watch']);
+// const build = gulp.series(sass);
+
+// // gulp.task('build', gulp.series('sass', 'concat', 'minify', 'smoosh'));
+// // gulp.task('default', gulp.series('build', 'watch'));
+
+// exports.build = build;
+
+// exports.default = build;
+
+const { series, src, dest } = require("gulp");
+const gulpSass = require("gulp-sass");
+const gulpConcat = require("gulp-concat");
+const gulpMinifyCSS = require("gulp-minify-css");
+const gulpRename = require("gulp-rename");
+const gulpSmoosher = require("gulp-smoosher");
+const gulpWatch = require("gulp-watch");
+
+gulpSass.compiler = require("node-sass");
+
+const sassSrc = "./src/sass/**/*.{scss,sass}";
+const cssSrc = "./src/css/**/*.css";
+
+function sass() {
+  return src(sassSrc)
+    .pipe(gulpSass().on("error", gulpSass.logError))
+    .pipe(dest("src/css"));
+}
+
+function concat() {
+  return src(cssSrc).pipe(gulpConcat("styles.css")).pipe(dest("./css/"));
+}
+
+function minify() {
+  return src("./css/styles.css").pipe(gulpMinifyCSS()).pipe(dest("./css/"));
+}
+
+function rename() {
+  return src("./css/styles.css")
+    .pipe(gulpRename("styles.min.css"))
+    .pipe(dest("./css/"));
+}
+
+function smoosh() {
+  return src("./src/index.html").pipe(gulpSmoosher()).pipe(dest("./"));
+}
+
+function watch() {
+  return gulpWatch([sassSrc, "./src/index.html"], function () {
+    series(sass, concat, rename, smoosh);
+  });
+}
+
+exports.default = series(sass, concat, rename, smoosh);
